0ebd61108f8d4d0d3cc7aa9c19f43be68b4898bc
Zabil Maooz <zabil.coder@gmail.com>
2021-04-03 12:06:03 +0500

    test

--- Flash_LED/Flash_LED.ino
+++ Flash_LED/Flash_LED.ino
@@ -1,15 +1,16 @@
 int led_pin = 3;
-void setup() {
+void setup()
+{
   pinMode(led_pin, OUTPUT);
-
 }
 
-void loop() {
-  digitalWrite(led_pin, HIGH);  
+void loop()
+{
+  digitalWrite(led_pin, HIGH);
   delay(200);
   digitalWrite(led_pin, LOW);
   delay(200);
-  digitalWrite(led_pin, HIGH);  
+  digitalWrite(led_pin, HIGH);
   delay(200);
   digitalWrite(led_pin, LOW);
   delay(1400);
